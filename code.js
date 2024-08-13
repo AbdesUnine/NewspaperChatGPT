8e6117b613b3205861f43aaa16b8976d5c562d80
Abdes <abdessalam.ouaazki@unine.ch>
2024-08-13 17:26:07 +0200

    Qualtrics final experiment

--- code.js
+++ code.js
@@ -26,7 +26,7 @@
 	function redirectToQualtrics() {
 		if (typeof startTime !== 'undefined') {
 			let params = getSpecificQueryParams();
-			let baseUrl = 'https://neuchatel.eu.qualtrics.com/jfe/form/SV_dpeVpKSyWb7Vm5g';
+			let baseUrl = 'https://neuchatel.eu.qualtrics.com/jfe/form/SV_e8wsavCiIKQgmFw';
 			let timeSpent = calculateTimeOnSite();
 
 			if (timeSpent > 30) {
